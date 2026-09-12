f1b1e7632997b332e3a46cb0e8f126271d84088a
ibararestiguevaldez-jpg <ibar.arestigue.valdez@gmail.com>
2026-09-12 10:07:43 -0300

    Create health.js

---
+++ api/health.js
@@ -0,0 +1,15 @@
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  res.status(200).json({ 
+    status: 'OK', 
+    timestamp: new Date().toISOString() 
+  });
+};
